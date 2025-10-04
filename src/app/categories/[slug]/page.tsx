7c291cb005e3cf87c58ed418336b68cf631bd499
Mo45 <kirillkrasin@gmail.com>
2025-10-04 19:19:08 +0300

    searchParams fix

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -151,13 +151,12 @@ export default async function CategoryPage(props: { params: Promise<{ slug: stri
                 {image && (
                   <div className="w-full relative">
                     <Image 
-                      src={`${process.env.NEXT_PUBLIC_BACKEND}${imageFormats.large?.url || imageFormats.medium?.url || imageFormats.small?.url || image.url}`}
+                      src={`${process.env.NEXT_PUBLIC_BACKEND}${imageFormats.large?.url || image.url}`}
                       decoding="async"
                       loading="lazy"
                       alt={image.alternativeText || article.title}
                       className="w-full h-full object-cover"
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      fill
                     />
                   </div>
                 )}
